94ac1232e6b29b7bfb4b8190b57bb22201979a1b
Akhmad Faizal <73004952+akhfzz@users.noreply.github.com>
2021-04-20 10:55:20 +0700

    Update animate2.js

--- Prosses/static/js/animate2.js
+++ Prosses/static/js/animate2.js
@@ -16,21 +16,15 @@ $(document).ready(function(){
 //         });
 //     });
 // });
-function populateSelect() {
-        // THE JSON ARRAY.
-        var mydata = JSON.parse(provinsi);
-        var ele = document.getElementById('daerah');
-        for (var i = 0; i < mydata.length; i++) {
-        // POPULATE SELECT ELEMENT WITH JSON.
-            ele.innerHTML = ele.innerHTML +
-                '<option value="' + mydata[i]['id'] + '">' + mydata[i]['nama'] + '</option>';
-        }
-     }
-function show(ele) {
-        // GET THE SELECTED VALUE FROM <select> ELEMENT AND SHOW IT.
-        var msg = document.getElementById('did');
-        msg.innerHTML = 'Selected city: <b>' + ele.options[ele.selectedIndex].text + '</b> </br>' +
-            'ID: <b>' + ele.value + '</b>';
+function show() {
+        var txt, data, i
+	txt = "<select>";
+	data = JSON.parse(provinsi);
+	for (i = 0; i < data.length; i++){
+	txt += "<option value = " +  data[i]["id"] + ">" + data[i]['nama'] + '</option>'
+	};
+	txt += "</select>"
+	document.getElementById("daerah").innerHTML = txt
     }
 function myFile(){
     var file = document.createElement("INPUT");
